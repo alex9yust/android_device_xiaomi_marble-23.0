c409aa1f5b95ab3245140a6fe841ad553d877e0b
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-23 01:17:03 +0200

    marble: keylayout: Copy using Soong
    Change-Id: I14fa8d6805b09c7c896721ff602133e7c7d2d18b
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- device.mk
+++ device.mk
@@ -230,9 +230,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
 # Keylayout
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
-    $(LOCAL_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
+PRODUCT_PACKAGES += \
+    uinput-fpc.kl \
+    uinput-goodix.kl
 
 # Keymaster
 PRODUCT_COPY_FILES += \
