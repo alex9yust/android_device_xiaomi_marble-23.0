fa6dd5e011e00148eba881d4b6c332105ed62b7c
Alex Yustasov <alex9yust@gmail.com>
2025-09-29 21:17:20 +0300

    marble: fix auto brightness after screen turn on

--- sensors/notifiers/RawLightNotifier.h
+++ sensors/notifiers/RawLightNotifier.h
@@ -8,6 +8,14 @@
 
 #include "SensorNotifier.h"
 
+enum notify_t {
+    BRIGHTNESS = 17,
+    DC_STATE = 18,
+    DISPLAY_FREQUENCY = 20,
+    REPORT_VALUE = 201,
+    POWER_STATE = 202,
+};
+
 class RawLightNotifier : public SensorNotifier {
   public:
     RawLightNotifier(sp<ISensorManager> manager);
@@ -15,4 +23,6 @@ class RawLightNotifier : public SensorNotifier {
 
   protected:
     void notify();
+
+    bool isEnable;
 };
